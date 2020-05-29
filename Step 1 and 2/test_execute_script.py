8c4101041554f781bc6e39f79fcc7594612ac652
Vladislav Vasilev <vladimirvlad-68@mail.ru>
2020-05-29 21:50:07 +0300

    +add done tasks

---
+++ Step 1 and 2/test_execute_script.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import time
+import math
+from selenium.webdriver.support.ui import Select
+
+try:
+    link = "http://suninjuly.github.io/selects1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x = browser.find_element_by_id('num1').text
+    y = browser.find_element_by_id('num2').text
+
+
+    # Ваш код, который заполняет обязательные поля
+    select = Select(browser.find_element_by_tag_name("select"))
+    select.select_by_value( str(int(x) + int(y)))
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+
+
+finally:
+
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(20)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
---
+++ Step 1 and 2/test_scroll.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import time
+import math
+from selenium.webdriver.support.ui import Select
+
+try:
+    link = " http://SunInJuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+
+    browser.execute_script("window.scrollBy(0, 100);")
+
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_id('answer')
+    input1.send_keys(y)
+    input2 = browser.find_element_by_id('robotCheckbox')
+    input2.click()
+    input3 = browser.find_element_by_id('robotsRule')
+    input3.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+
+
+finally:
+
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(20)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
